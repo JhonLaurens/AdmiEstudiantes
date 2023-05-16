05bcc28b0565c0acd57ac37d16a9a370db4d1597
JhonLaurens <jhonlaurens@gmail.com>
2023-05-16 18:55:34 -0500

    ok

--- PL/Form1.Designer.cs
+++ PL/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEstudiantes = new System.Windows.Forms.Button();
+            this.btnProgramas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(225, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(324, 320);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(134, 51);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Cerrar Aplicación";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnEstudiantes
+            // 
+            this.btnEstudiantes.Location = new System.Drawing.Point(452, 189);
+            this.btnEstudiantes.Name = "btnEstudiantes";
+            this.btnEstudiantes.Size = new System.Drawing.Size(115, 89);
+            this.btnEstudiantes.TabIndex = 5;
+            this.btnEstudiantes.Text = "Estudiantes";
+            this.btnEstudiantes.UseVisualStyleBackColor = true;
+            // 
+            // btnProgramas
+            // 
+            this.btnProgramas.Location = new System.Drawing.Point(206, 189);
+            this.btnProgramas.Name = "btnProgramas";
+            this.btnProgramas.Size = new System.Drawing.Size(117, 89);
+            this.btnProgramas.TabIndex = 4;
+            this.btnProgramas.Text = "Programas";
+            this.btnProgramas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEstudiantes);
+            this.Controls.Add(this.btnProgramas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEstudiantes;
+        private System.Windows.Forms.Button btnProgramas;
     }
 }
 
